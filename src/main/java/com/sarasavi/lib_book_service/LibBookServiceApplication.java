daecf1dc61baf44431fced7194759698e79e2c82
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-16 09:46:01 +0530

    init:  project structure with Maven config 🚀

---
+++ src/main/java/com/sarasavi/lib_book_service/LibBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_book_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibBookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarasavi/lib_book_service/LibBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
