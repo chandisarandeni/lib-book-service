a67e274461c8883f95f7655a358b4cfbe1f54a14
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-17 13:49:35 +0530

    feat: add RestTemplate Bean ✅

--- src/main/java/com/sarasavi/lib_book_service/LibBookServiceApplication.java
+++ src/main/java/com/sarasavi/lib_book_service/LibBookServiceApplication.java
@@ -10,6 +10,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LibBookServiceApplication {
@@ -23,4 +24,10 @@ public class LibBookServiceApplication {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
